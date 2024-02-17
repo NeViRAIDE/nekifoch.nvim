bcc1bfec998b98786e37141b7d6d50ba9ff8dc60
RAprogramm <andrey.rozanov.vl@gmail.com>
2024-02-17 18:11:34 +0900

    add: nui set size

--- lua/nekifoch/which_key.lua
+++ lua/nekifoch/which_key.lua
@@ -5,5 +5,8 @@ wk.register({
   ['<leader>sfl'] = { ':Nekifoch list<cr>', 'Fonts list' },
   ['<leader>sfc'] = { ':Nekifoch check<cr>', 'Check current font settings' },
   ['<leader>sff'] = { ':Nekifoch set_font<cr>', 'Set font family' },
-  ['<leader>sfs'] = { ':Nekifoch set_size<cr>', 'Set font size' },
+  ['<leader>sfs'] = {
+    function() require('nekifoch.nui_set_size')() end,
+    'Set font size',
+  },
 })
