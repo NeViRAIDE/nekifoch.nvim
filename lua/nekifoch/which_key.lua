2130efcd274d4ca658eb0204099149187672449a
RAprogramm <andrey.rozanov.vl@gmail.com>
2024-07-13 16:18:22 +0900

    which-key v3

--- lua/nekifoch/which_key.lua
+++ lua/nekifoch/which_key.lua
@@ -1,15 +1,9 @@
 local wk = require('which-key')
 
-wk.register({
-  ['<leader>sf'] = { name = 'Font' },
-  ['<leader>sfl'] = { ':Nekifoch list<cr>', 'Fonts list' },
-  ['<leader>sfc'] = { ':Nekifoch check<cr>', 'Check current font settings' },
-  ['<leader>sff'] = {
-    function() require('nekifoch.nui_set_font')() end,
-    'Set font family',
-  },
-  ['<leader>sfs'] = {
-    function() require('nekifoch.nui_set_size')() end,
-    'Set font size',
-  },
+wk.add({
+  {'<leader>sf',  group = 'Font' },
+  {'<leader>sfl' ,  '<cmd>Nekifoch list<cr>', desc = 'Fonts list' },
+  {'<leader>sfc' ,  '<cmd>Nekifoch check<cr>', desc = 'Check current font settings' },
+  {'<leader>sff', rhs = function() require('nekifoch.nui_set_font')() end, desc = 'Set font family' },
+  {'<leader>sfs', rhs = function() require('nekifoch.nui_set_size')() end, desc = 'Set font size' },
 })
